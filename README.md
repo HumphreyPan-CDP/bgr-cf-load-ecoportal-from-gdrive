cef193194bf742d1c601f3506cc2b711c7a505e7
Humphrey Pan <94580603+HumphreyPan-CDP@users.noreply.github.com>
2024-04-10 11:18:21 +1200

    fix

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ gcloud functions deploy load-ecoportal-from-gdrive `
         "target_dataset_name" : "ecoportal",
         "target_default_partition" : "1900-01-01"
 }
+
+
+dev folder: [{'webViewLink': 'https://drive.google.com/drive/folders/1V9txunznrUuhMiNNfE57V8_5lUj-g316', 'id': '1V9txunznrUuhMiNNfE57V8_5lUj-g316', 'name': 'DEV', 'modifiedTime': '2024-04-09T21:22:39.778Z'}]
+
+NPD:
+[{'webViewLink': 'https://drive.google.com/drive/folders/1aneXjhOi-iHrdkXKVwX4kr94vKQ9_eBc', 'id': '1aneXjhOi-iHrdkXKVwX4kr94vKQ9_eBc', 'name': 'NPD', 'modifiedTime': '2024-04-09T21:27:01.033Z'}]
+
+prod
+[{'webViewLink': 'https://drive.google.com/drive/folders/112RIRRZNqPeU2oa2BgXlWxjkYglIpUrM', 'id': '112RIRRZNqPeU2oa2BgXlWxjkYglIpUrM', 'name': 'PROD', 'modifiedTime': '2024-04-08T22:54:30.531Z'}]
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 import functions_framework
 
+DOWDLOADED_FLAG = "downloaded"
 
 def search_file(query, creds):
     """Search file in drive location
@@ -26,17 +27,17 @@ def search_file(query, creds):
             # pylint: disable=maybe-no-member
             # response = service.files().list(q=query, spaces='drive', fields='nextPageToken, ' 'files(id, name, modifiedTime, description)', pageToken=page_token).execute()
             response = service.files().list(q=query, spaces='drive', 
-                                            fields='nextPageToken, ' 'files(id, name, modifiedTime, description, webContentLink, webViewLink)', 
+                                            fields='nextPageToken, ' 'files(id, name, modifiedTime, description, webContentLink, webViewLink, capabilities)', 
                                             pageToken=page_token).execute()
             for file in response.get('files', []):
-                # Process change
-                print(F'Found file: {file.get("name")}, {file.get("id")}, {file.get("description")}')
+                # print(f'Found file: {file.get("name")}, {file.get("id")}, {file.get("description")}, {file.get("capabilities")}')
+                print(f'Found file: {file.get("name")}, {file.get("id")}, {file.get("description")}')
             files.extend(response.get('files', []))
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
     except HttpError as error:
-        print(F'An error occurred: {error}')
+        print(f'\t An error occurred: {error}')
         files = None
     return files
 
@@ -49,9 +50,9 @@ def download_file_from_drive(file_id, creds):
         done = False
         while done is False:
             status, done = downloader.next_chunk()
-            print(F'Download {int(status.progress() * 100)}.')
+            print(f'\t Download {int(status.progress() * 100)}.')
     except HttpError as error:
-        print(F'An error occurred: {error}')
+        print(f'\t An error occurred: {error}')
         file = None
     return file.getvalue()
 
@@ -62,7 +63,7 @@ def upload_file_to_bkt(bkt_project, bkt_name, file_name, file):
     bucket = storage_client.bucket(bkt_name)
     blob = bucket.blob(file_name)
     blob.upload_from_string(file)
-    print(f"file {file_name} uploaded to {bkt_name}")
+    print(f"\t file {file_name} uploaded to {bkt_name}")
 
 def create_bigquery_extranl_table(project, dataset, table_name, file_prefix, external_source_format):
     client = bigquery.Client(project)
@@ -102,22 +103,26 @@ def create_bigquery_extranl_table(project, dataset, table_name, file_prefix, ext
     except Conflict:
         print(f"table existed: {table}")
 
-# def update_gdrive_file(source_file, target_name, file_id, creds):
-#     try:
-#         service = build('drive', 'v3', credentials=creds)
-#         file_metadata = {'name': target_name}
-#         media = MediaFileUpload(source_file, mimetype='text/csv')
-#         file = service.files().update(body=file_metadata, media_body=media, fileId=file_id).execute()
-#         print(F'File ID: {file.get("id")}')
-#     except HttpError as error:
-#         print(F'An error occurred: {error}')
-#         file = None
+def normalise_column_name(col_names):
+    result = [re.sub('[^0-9a-zA-Z]+', '_', col.lower()) for col in col_names]
+    result = [re.sub('^[0-9]+', '', col) for col in result]
+    result = [re.sub('[^0-9a-zA-Z]+$', '', col) for col in result]
+    result= [re.sub('^[^0-9a-zA-Z]+', '', col) for col in result]
+    return result
+
+def rename_gdrive_file(file_id, new_name, creds):
+    try: 
+        service = build('drive', 'v3', credentials=creds)
+        body = {'name': new_name}
+        service.files().update(fileId=file_id, body=body).execute()
+        print(f"\t file {file_id}| is renamed to {new_name}")
+    except HttpError as error:
+        print(f'\t An error occurred when renaming file: {file_id}')
 
 @functions_framework.http
 def load_flie_from_gdrive(request):    
     try: 
         request_json = request.get_json(silent=True)
-
         gdrive_folder_id = request_json['gdrive_folder_id']
         landing_project = request_json['landing_project']
         target_bucket_name = request_json['target_bucket_name']
@@ -126,7 +131,7 @@ def load_flie_from_gdrive(request):
         target_dataset_name = request_json['target_dataset_name']
         target_default_partition = request_json['target_default_partition']
 
-        # gdrive_folder_id = "112RIRRZNqPeU2oa2BgXlWxjkYglIpUrM"
+        # gdrive_folder_id = "1V9txunznrUuhMiNNfE57V8_5lUj-g316"
         # landing_project = "bgr-d-landing-b23c"
         # target_bucket_name = "bgr-d-landing-file-history"
         # target_archive_bucket_name = "bgr-d-ecoportal-archive"
@@ -157,42 +162,47 @@ def load_flie_from_gdrive(request):
     files = search_file(query, creds)
 
     for table in target_tables:
-        print(f"dealing with file for table {table}")
-        files_to_load = [file for file in files if table in file['name'].lower()]
+        print(f"dealing with file for table: {table}")
+        
+        files_to_load = [file for file in files if (
+            (table in file['name'].lower()) and (file['name'].startswith(DOWDLOADED_FLAG)==False)
+            )
+            ]
         for file in files_to_load:
-            print(f"dealing file: {file['name']}")
+            file_name = file['name']
+            print(f"dealing file: {file_name}".center(100, '*'))
             
             # parse hive partition value from file suffix
-            name_suffix = file['name'].lower().replace(table, '').replace('.csv', '').replace('-', '')
+            name_suffix = file_name.lower().replace(table, '').replace('.csv', '').replace('-', '')
             try:
                 partition = datetime.strptime(name_suffix, '%Y%m%d').date().strftime('%Y-%m-%d')
             except ValueError:
-                print(f"name_suffix of file {file['name']} is not a date, will put file in to default partition {target_default_partition}")
+                print(f"\t name_suffix of file {file_name} is not a date, will put file in to default partition {target_default_partition}")
                 partition = target_default_partition
 
             # download file from Google drive
-            file_id = file['id']
-            file_data = download_file_from_drive(file_id, creds)
+            file_data = download_file_from_drive(file['id'], creds)
 
             # save raw csv to archive bucket
-            upload_file_to_bkt(landing_project, target_archive_bucket_name, f"{table}/{file['name']}", file_data)
+            upload_file_to_bkt(landing_project, target_archive_bucket_name, f"{table}/{file_name}", file_data)
 
-            # save raw csv to hive-partition bucket
             # remove special character from column names
             file_df = pd.read_csv(io.StringIO(file_data.decode("utf-8")))
-            file_df.columns = [re.sub('[^0-9a-zA-Z]+', '_', col.lower()) for col in file_df.columns]
-            file_df.columns = [re.sub('^[0-9]+', '', col) for col in file_df.columns]
-            file_df.columns = [re.sub('[^0-9a-zA-Z]+$', '', col) for col in file_df.columns]
-            file_df.columns = [re.sub('^[^0-9a-zA-Z]+', '', col) for col in file_df.columns]
+            file_df.columns = normalise_column_name(file_df.columns)
             file_df = file_df.astype(str)
-            print(f"rows_in_file: {len(file_df)}")
-            target_file_name = f"{table}/ingestion_date={partition}/{file['name'].lower().replace('.csv', '.parquet')}"
-            file_df.to_parquet(f"gs://{target_bucket_name}/{target_file_name}")
+            print(f"\t rows_in_file: {len(file_df)}")
+
+            # save file to hive-partition bucket
+            target_file_name = f"{table}/ingestion_date={partition}/{file_name.lower().replace('.csv', '.parquet')}"
+            target_blob = f"gs://{target_bucket_name}/{target_file_name}"
+            file_df.to_parquet(target_blob)
+            print(f"\t file {target_file_name} uploaded to {target_blob}")
+
+            rename_gdrive_file(file['id'], f'{DOWDLOADED_FLAG}_{file_name}', creds)
 
         # create bigquery external table
         file_prefix = f"gs://{target_bucket_name}/{table}"
         create_bigquery_extranl_table(landing_project, target_dataset_name, table, file_prefix, "PARQUET")
 
-    response = 'OK', 200
-    return response
+    return 'OK', 200
 
